a18e21228a0887adf7116ae02c541cce6b572f46
mahdibou96 <38530687+mahdibou96@users.noreply.github.com>
2019-11-07 07:40:12 +0100

    Add files via upload

---
+++ OffreController.cs
@@ -0,0 +1,131 @@
+﻿using Data;
+using Domain.entities;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class OffreController : Controller
+    {
+        // GET: oooo
+        OffreService OS;
+        ProduitService PS;
+
+        public OffreController()
+        {
+            OS = new OffreService();
+            PS = new ProduitService();
+        }
+
+        [HttpGet]
+        public ActionResult Index(OffreModels om)
+        {
+          return View(new OffreModels
+            {
+                offre = new Offre(),
+                produit = new Produit(),
+                ListProduits = PS.GetAll()
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateOffre(OffreModels om)
+        {
+            if (ModelState.IsValid)
+            {
+                Produit pp = new Produit();
+
+                using (Context conn = new Context())
+                {
+                    foreach (var item in conn.Product)
+                    {
+                        pp = item;
+                    }
+
+                    om.offre.NouveauPrix = pp.Prix - ((pp.Prix * om.offre.PourcentageDeReduction) / 100);
+                  //  OS.Add(om.offre);
+                    //OS.Commit();
+                    conn.Offres.Add(om.offre);
+                    conn.SaveChanges();
+                    return RedirectToAction("Affichage", "Offre");
+                }
+
+            }
+            else
+                return RedirectToAction("Index", "Offre");
+
+        }
+
+        public ActionResult Affichage()
+        {
+            return View(OS.GetAll());
+
+        }
+
+        // GET: Offre/Edit/2
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(OS.GetById(id));
+        }
+        //*124# offre
+        // POST: Offre/AddOrEdit/2
+        [HttpPost]
+        public ActionResult Edit(int id, Offre p)
+        {
+            if (ModelState.IsValid)
+            {
+                Offre x = new Offre();
+
+                x = OS.GetAll().Where(a => a.IdOffresProduit == p.IdOffresProduit).FirstOrDefault();
+
+                x.NouveauPrix = p.NouveauPrix;
+                x.PourcentageDeReduction = p.PourcentageDeReduction;
+                x.QtDeProduit = p.QtDeProduit;
+                x.DateDebut = p.DateDebut;
+                x.DateFin = p.DateFin;
+
+                OS.Update(x);
+                OS.Commit();
+
+                return RedirectToAction("Affichage");
+            }
+            else
+                return RedirectToAction("Index");
+        }
+
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            return View(OS.GetById(id));
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Offre p)
+        {
+            p = OS.GetById(id);
+            OS.Delete(p);
+            OS.Commit();
+            return RedirectToAction("Affichage");
+
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(OS.GetById(id));
+            //return View();
+
+        }
+
+    }
+}
---
+++ PackController.cs
@@ -0,0 +1,130 @@
+﻿using Data;
+using Domain.entities;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PackController : Controller
+    {
+        PackService PS;
+        ProduitService ProduitS;
+        Context db = new Context();
+
+        public PackController()
+        {
+            PS = new PackService();
+            ProduitS = new ProduitService();
+        }
+
+        // GET: Pack
+        [HttpGet]
+        public ActionResult Index(PackModels PM)
+        {
+
+            var Entreprise = ProduitS.GetAll();
+
+            ViewBag.EntrepriseList = new SelectList(Entreprise, "IdProduit", "Name");
+
+            return View(new PackModels
+            {
+                IDPack = new Pack(),
+                ListProduits = ProduitS.GetAll()
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreatePack(Pack PM, Produit PP, PackModels pmm)
+        {
+            if (ModelState.IsValid)
+            {
+                using (Context conn = new Context())
+                {
+                    Produit p = new Produit();
+
+                    var x = pmm.IdProduitsFk;
+                    double prix = 0;
+                    foreach (var it in x)
+                    {
+                        foreach (var item in conn.Product)
+                        {
+                            if (item.IdProduit == it)
+                            {
+                                p = item;
+                                prix = prix + item.Prix;
+                            }
+                        }
+
+                        pmm.IDPack.PrixTotaleProduitorig = prix;
+                        pmm.IDPack.NouveauPrixTotaleProduit = prix - ((prix * pmm.IDPack.PourcentageDeReduction) / 100);
+                        pmm.IDPack.Produits.Add(p);
+                        conn.packs.Add(pmm.IDPack);
+                    }
+                    conn.SaveChanges();
+
+                }
+                
+                return RedirectToAction("Index", "Pack");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Offre");
+
+            }
+        }
+
+
+        public ActionResult Affichage()
+        {
+            return View(PS.GetAll());
+
+        }
+
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            return View(PS.GetById(id));
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Pack p)
+        {
+            p = PS.GetById(id);
+            PS.Delete(p);
+            PS.Commit();
+            return RedirectToAction("Affichage");
+
+        }
+        
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {PackModels pm = new PackModels();
+            var x = PS.GetById(id);
+            pm.ListProduits = x.Produits;
+            return View(pm);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+    
